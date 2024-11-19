9768e1e1b8cfb860f5e2339f3baa45cfb699a5c6
1022gusl <1022gusl@gmail.com>
2024-11-19 09:59:12 +0900

    style: 스타일 적용

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import { useEffect, useState } from "react";
 import ProductList from "@/components/ProductList";
 import SearchForm from "@/components/SearchForm";
 import axios from "@/lib/axios";
-import { useEffect, useState } from "react";
+import styles from "@/styles/Home.module.css";
+import Header from "@/components/Header";
+import Container from "@/components/Container";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
@@ -18,9 +21,11 @@ export default function Home() {
 
   return (
     <>
-      <h1>Codeitmall</h1>
-      <SearchForm />
-      <ProductList products={products} />
+      <Header />
+      <Container>
+        <SearchForm />
+        <ProductList className={styles.products} products={products} />
+      </Container>
     </>
   );
 }
